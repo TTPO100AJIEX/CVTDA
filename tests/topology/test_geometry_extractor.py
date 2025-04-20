4a8ddd85791ca0b38a403df06358b96df25e23d4
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2025-04-21 01:06:29 +0300

    Validation, benchmarks, tests, reade & publsih to PyPI

---
+++ tests/topology/test_geometry_extractor.py
@@ -0,0 +1,76 @@
+import numpy
+import pytest
+import skimage.data
+
+import cvtda.topology
+
+def make_rgb():
+    return skimage.transform.resize(skimage.data.astronaut(), (32, 32))
+
+def make_gray():
+    return make_rgb()[:, :, 0]
+
+
+def test_gray_reduced():
+    input = numpy.array([ make_gray() ])
+    output = cvtda.topology.GeometryExtractor(n_jobs = 1).fit_transform(input)
+    assert output.shape == (1, 2617)
+    assert numpy.isnan(output).sum() == 0
+    
+def test_gray_full():
+    input = numpy.array([ make_gray() ])
+    output = cvtda.topology.GeometryExtractor(n_jobs = 1, reduced = False).fit_transform(input)
+    assert output.shape == (1, 4657)
+    assert numpy.isnan(output).sum() == 0
+    
+def test_rgb_reduced():
+    input = numpy.array([ make_rgb() ])
+    output = cvtda.topology.GeometryExtractor(n_jobs = 1).fit_transform(input)
+    assert output.shape == (1, 2617 * 4 + 1019)
+    assert numpy.isnan(output).sum() == 0
+    
+def test_rgb_full():
+    input = numpy.array([ make_rgb() ])
+    output = cvtda.topology.GeometryExtractor(n_jobs = 1, reduced = False).fit_transform(input)
+    assert output.shape == (1, 4657 * 6 + 1019)
+    assert numpy.isnan(output).sum() == 0
+
+def test_batch():
+    input = numpy.array([ make_gray(), make_gray() ])
+    output = cvtda.topology.GeometryExtractor(n_jobs = 1).fit_transform(input)
+    assert output.shape == (2, 2617)
+    assert numpy.isnan(output).sum() == 0
+
+def test_constant_pixels():
+    input = numpy.zeros((1, 32, 32, 3))
+    output = cvtda.topology.GeometryExtractor(n_jobs = 1, reduced = False).fit_transform(input)
+    assert output.shape == (1, 4657 * 6 + 1019)
+    assert numpy.isnan(output).sum() == 0
+
+
+def test_transform_before_fit():
+    input = numpy.array([ make_gray() ])
+    with pytest.raises(AssertionError):
+        cvtda.topology.GeometryExtractor(n_jobs = 1).transform(input)
+
+def test_dimensions_mismatch():
+    input1 = numpy.array([ make_gray() ])
+    extractor = cvtda.topology.GeometryExtractor(n_jobs = 1).fit(input1)
+    
+    input2 = numpy.array([ make_rgb() ])
+    with pytest.raises(AssertionError):
+        extractor.transform(input2)
+
+@pytest.mark.parametrize(
+    ['shape'],
+    [
+        pytest.param((2, 16, 16, 16, 3), id = 'too many dimensions'),
+        pytest.param((2, 16), id = 'too few dimensions'),
+        pytest.param((2, 16, 16, 4), id = 'too many channels'),
+        pytest.param((2, 16, 16, 2), id = 'too few channels')
+    ]
+)
+def test_weird_shape(shape):
+    input1 = numpy.random.rand(*shape)
+    with pytest.raises(AssertionError):
+        cvtda.topology.GeometryExtractor(n_jobs = 1).fit(input1)
